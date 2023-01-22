0f8e25820666a0d7bd9c0f28df3beaa9e0b60e07
John K <jklacynski@outlook.com>
2023-01-22 06:54:40 -0500

    added comment

--- parse-image-lambda.py
+++ parse-image-lambda.py
@@ -6,12 +6,12 @@ def lambda_handler(event, context):
     # Get the object from the event
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = event['Records'][0]['s3']['object']['key']
-    
+
     # Set up client for Textract
     textract = boto3.client('textract')
     s3 = boto3.client('s3')
     dynamodb = boto3.resource('dynamodb')
-    
+
     # Extract text from image file
     response = textract.detect_document_text(
         Document={'S3Object': {'Bucket': bucket, 'Name': key}})
@@ -20,8 +20,8 @@ def lambda_handler(event, context):
         if block['BlockType'] == 'LINE':
             text += block['Text'] + '\n'
     print(text)
-    
-    # Insert the extracted text into DynamoDB
+
+    # Insert the extracted text into DynamoDB. Create DynamoDB table if it doesn't exist
     table = dynamodb.Table('TABLE_NAME')
     table.put_item(
         Item={
@@ -29,10 +29,8 @@ def lambda_handler(event, context):
             'text': text
         }
     )
-    
+
     # Copy object to another location
     copy_source = {'Bucket': bucket, 'Key': key}
-    s3.copy_object(Bucket='BUCKET', CopySource=copy_source, Key='DESTINATION_KEY/'+key)
-
-
-
+    s3.copy_object(Bucket='BUCKET', CopySource=copy_source,
+                   Key='DESTINATION_KEY/'+key)
